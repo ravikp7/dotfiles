43f63c626116973cd2f0a49939cfb0d8823ff4c9
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-12-11 14:17:05 +0530

    update config

---
+++ backup-sync.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Pulling latest from upstream..."
+cd ~/config-backup
+git pull origin master
+
+echo "Copying files"
+echo "Dotfiles"
+cd ~
+cp -t ~/config-backup/dotfiles/ .vimrc .bashrc .tmux.conf .gitconfig
+echo "Copied dotfiles"
+
+cd ~/config-backup
+
+echo "Determining changes"
+git diff-index --quiet HEAD
+if [$? == 0] ; then
+  echo "No changes. Exiting!"
+else
+  echo "Pushing new changes..."
+  git add .
+  git commit -m "update config"
+  git push origin master
+  echo "Pushed all chages"
+fi
+
---
+++ dotfiles/.vimrc
@@ -0,0 +1,36 @@
+" Map Colemak keys to QWERTY keys (in alphabetical order).
+noremap d g
+noremap e k
+noremap f e
+noremap g t
+noremap i l
+noremap j y
+noremap k n
+noremap l u
+noremap n j
+
+noremap p r
+noremap r s
+noremap s d
+noremap t f
+noremap u i
+noremap y o
+noremap D G
+noremap E K
+noremap F E
+noremap G T
+noremap I L
+noremap J Y
+noremap K N
+noremap L U
+noremap N J
+
+noremap P R
+noremap R S
+noremap S D
+noremap T F
+noremap U I
+noremap Y O
+
+" Set Tab to 2 spaces
+set sts=2
